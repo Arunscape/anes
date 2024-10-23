8f4bc96a3ab153205a934ceeb9a153400dfe0767
Arunscape <8227297+Arunscape@users.noreply.github.com>
2024-10-22 21:26:59 -0600

    wrote the opcodes

--- src/main.rs
+++ src/main.rs
@@ -252,6 +252,268 @@ static OPCODES: LazyLock<Vec<OpCode>> = LazyLock::new(|| {
         (0xC0, "CPY", 2, 2, AddressingMode::Immediate),
         (0xC4, "CPY", 2, 3, AddressingMode::ZeroPage),
         (0xCC, "CPY", 3, 4, AddressingMode::Absolute),
+        //
+        // DEC - Decrement Memory
+        (0xC6, "DEC", 2, 5, AddressingMode::ZeroPage),
+        (0xD6, "DEC", 2, 6, AddressingMode::ZeroPage_X),
+        (0xCE, "DEC", 3, 6, AddressingMode::Absolute),
+        (0xDE, "DEC", 3, 7, AddressingMode::Absolute_X),
+        //
+        // DEX - Decrement X Register
+        (0xCA, "DEX", 1, 2, AddressingMode::Implied),
+        //
+        // DEY - Decrement X Register
+        (0x88, "DEY", 1, 2, AddressingMode::Implied),
+        //
+        // EOR - Exclusive OR
+        (0x49, "EOR", 2, 2, AddressingMode::Immediate),
+        (0x45, "EOR", 2, 3, AddressingMode::ZeroPage),
+        (0x55, "EOR", 2, 4, AddressingMode::ZeroPage_X),
+        (0x4D, "EOR", 3, 4, AddressingMode::Absolute),
+        (
+            0x5D,
+            "EOR",
+            3,
+            4, // +1 if page crossed
+            AddressingMode::Absolute_X,
+        ),
+        (
+            0x59,
+            "EOR",
+            3,
+            4, // +1 if page crossed
+            AddressingMode::Absolute_Y,
+        ),
+        (0x41, "EOR", 2, 6, AddressingMode::Indirect_X),
+        (
+            0x51,
+            "EOR",
+            2,
+            5, // +1 if page crossed
+            AddressingMode::Indirect_Y,
+        ),
+        //
+        // INC - Increment Memory
+        (0xE6, "INC", 2, 5, AddressingMode::ZeroPage),
+        (0xF6, "INC", 2, 6, AddressingMode::ZeroPage_X),
+        (0xEE, "INC", 3, 6, AddressingMode::Absolute),
+        (0xFE, "INC", 3, 7, AddressingMode::Absolute_X),
+        //
+        // INX - Increment X Register
+        (0xE8, "INX", 1, 2, AddressingMode::Implied),
+        //
+        // INY - Increment Y Register
+        (0xC8, "INY", 1, 2, AddressingMode::Implied),
+        //
+        // JMP - Jump
+        (0x4C, "JMP", 3, 3, AddressingMode::Absolute),
+        (0x6C, "JMP", 3, 5, AddressingMode::Indirect),
+        //
+        // JSR - Jump to Subroutine
+        (0x20, "JSR", 3, 6, AddressingMode::Absolute),
+        //
+        // LDA - Load accumulator
+        (0xA9, "LDA", 2, 2, AddressingMode::Immediate),
+        (0xA5, "LDA", 2, 3, AddressingMode::ZeroPage),
+        (0xB5, "LDA", 2, 4, AddressingMode::ZeroPage_X),
+        (0xAD, "LDA", 3, 4, AddressingMode::Absolute),
+        (
+            0xBD,
+            "LDA",
+            3,
+            4, // +1 if page crossed
+            AddressingMode::Absolute_X,
+        ),
+        (
+            0xB9,
+            "LDA",
+            3,
+            4, // +1 if page crossed
+            AddressingMode::Absolute_Y,
+        ),
+        (0xA1, "LDA", 2, 6, AddressingMode::Indirect_X),
+        (
+            0xB1,
+            "LDA",
+            2,
+            5, // +1 if page crossed
+            AddressingMode::Indirect_Y,
+        ),
+        //
+        // LDX - Load X Register
+        (0xA2, "LDX", 2, 2, AddressingMode::Immediate),
+        (0xA6, "LDX", 2, 3, AddressingMode::ZeroPage),
+        (0xB6, "LDX", 2, 4, AddressingMode::ZeroPage_Y),
+        (0xAE, "LDX", 3, 4, AddressingMode::Absolute),
+        (
+            0xBE,
+            "LDX",
+            3,
+            4, //+1 if page crossed
+            AddressingMode::Absolute_Y,
+        ),
+        //
+        // LDY - Load Y Register
+        (0xA0, "LDY", 2, 2, AddressingMode::Immediate),
+        (0xA4, "LDY", 2, 3, AddressingMode::ZeroPage),
+        (0xB4, "LDY", 2, 4, AddressingMode::ZeroPage_X),
+        (0xAC, "LDY", 3, 4, AddressingMode::Absolute),
+        (
+            0xBC,
+            "LDY",
+            3,
+            4, //+1 if page crossed
+            AddressingMode::Absolute_X,
+        ),
+        //
+        // LSR - Logical Shift Right
+        (0x4A, "LSR", 1, 2, AddressingMode::Accumulator),
+        (0x46, "LSR", 2, 5, AddressingMode::ZeroPage),
+        (0x56, "LSR", 2, 6, AddressingMode::ZeroPage_X),
+        (0x4E, "LSR", 3, 6, AddressingMode::Absolute),
+        (0x5E, "LSR", 3, 7, AddressingMode::Absolute_X),
+        //
+        // NOP - No Operation
+        (0xEA, "NOP", 1, 2, AddressingMode::Implied),
+        //
+        // ORA - Logical Inclusive OR
+        (0x09, "ORA", 2, 2, AddressingMode::Immediate),
+        (0x05, "ORA", 2, 3, AddressingMode::ZeroPage),
+        (0x15, "ORA", 2, 4, AddressingMode::ZeroPage_X)(
+            0x0D,
+            "ORA",
+            3,
+            4,
+            AddressingMode::Absolute,
+        ),
+        (
+            0x1D,
+            "ORA",
+            3,
+            4, // +1 if page crossed
+            AddressingMode::Absolute_X,
+        ),
+        (
+            0x19,
+            "ORA",
+            3,
+            4, // +1 if page crossed
+            AddressingMode::Absolute_Y,
+        ),
+        (0x01, "ORA", 2, 6, AddressingMode::Indirect_X),
+        (
+            0x11,
+            "ORA",
+            2,
+            5, // +1 if page crossed
+            AddressingMode::Indirect_Y,
+        ),
+        //
+        // PHA - Push Accumulator
+        (0x48, "PHA", 1, 3, AddressingMode::Implied),
+        //
+        // PHP - Push Processor Status
+        (0x08, "PHP", 1, 3, AddressingMode::Implied),
+        //
+        // PLA - Pull Accumulator
+        (0x68, "PLA", 1, 4, AddressingMode::Implied),
+        //
+        // PLP - Pull Processor Status
+        (0x28, "PLP", 1, 4, AddressingMode::Implied),
+        //
+        // ROL - Rotate Left
+        (0x2A, "ROL", 1, 2, AddressingMode::Accumulator),
+        (0x26, "ROL", 2, 5, AddressingMode::ZeroPage),
+        (0x36, "ROL", 2, 6, AddressingMode::ZeroPage_X),
+        (0x2E, "ROL", 3, 6, AddressingMode::Absolute),
+        (0x3E, "ROL", 3, 7, AddressingMode::Absolute_X),
+        //
+        // ROR - Rotate Right
+        (0x2A, "ROR", 1, 2, AddressingMode::Accumulator),
+        (0x26, "ROR", 2, 5, AddressingMode::ZeroPage),
+        (0x36, "ROR", 2, 6, AddressingMode::ZeroPage_X),
+        (0x2E, "ROR", 3, 6, AddressingMode::Absolute),
+        (0x3E, "ROL", 3, 7, AddressingMode::Absolute_X),
+        //
+        // RTI - Return from Interrupt
+        (0x40, "RTI", 1, 6, AddressingMode::Implied),
+        //
+        // RTS - Return from Subroutine
+        (0x60, "RTS", 1, 6, AddressingMode::Implied),
+        //
+        // SBC - Subtract with Carry
+        (0xE9, "SBC", 2, 2, AddressingMode::Immediate),
+        (0xE5, "SBC", 2, 3, AddressingMode::ZeroPage),
+        (0xF5, "SBC", 2, 4, AddressingMode::ZeroPage_X),
+        (0xED, "SBC", 3, 4, AddressingMode::Absolute),
+        (
+            0xFD,
+            "SBC",
+            3,
+            4, // +1 if page crossed
+            AddressingMode::Absolute_X,
+        ),
+        (
+            0xF9,
+            "SBC",
+            3,
+            4, // +1 if page crossed
+            AddressingMode::Absolute_Y,
+        ),
+        (0xE1, "SBC", 2, 6, AddressingMode::Indirect_X),
+        (
+            0xF1,
+            "SBC",
+            2,
+            5, // +1 if page crossed
+            AddressingMode::Indirect_Y,
+        ),
+        //
+        // SEC - Set Carry Flag
+        (0x38, "SEC", 1, 2, AddressingMode::Implied),
+        //
+        // SED - Set Decimal Flag
+        (0xF8, "SED", 1, 2, AddressingMode::Implied),
+        //
+        // SEI - Set Inturrupt Disable
+        (0x78, "SEI", 1, 2, AddressingMode::Implied),
+        //
+        // STA - Store Accumulator
+        (0x85, "STA", 2, 3, AddressingMode::ZeroPage),
+        (0x95, "STA", 2, 4, AddressingMode::ZeroPage_X),
+        (0x8D, "STA", 3, 4, AddressingMode::Absolute),
+        (0x9D, "STA", 3, 5, AddressingMode::Absolute_X),
+        (0x99, "STA", 3, 5, AddressingMode::Absolute_Y),
+        (0x81, "STA", 2, 6, AddressingMode::Indirect_X),
+        (0x91, "STA", 2, 6, AddressingMode::Indirect_Y),
+        //
+        // STX - Store X Register
+        (0x86, "STX", 2, 3, AddressingMode::ZeroPage),
+        (0x96, "STX", 2, 4, AddressingMode::ZeroPage_Y),
+        (0x8E, "STX", 3, 4, AddressingMode::Absolute),
+        //
+        // STY - Store Y Register
+        (0x84, "STY", 2, 3, AddressingMode::ZeroPage),
+        (0x94, "STY", 2, 4, AddressingMode::ZeroPage_X),
+        (0x8C, "STY", 3, 4, AddressingMode::Absolute),
+        //
+        // TAX - Transfer Accumulator to X
+        (0xAA, "TAX", 1, 2, AddressingMode::Implied),
+        //
+        // TAY - Transfer Accumulator to Y
+        (0xA8, "TAY", 1, 2, AddressingMode::Implied),
+        //
+        // TSX - Transfer Stack Pointer to X
+        (0xBA, "TSX", 1, 2, AddressingMode::Implied),
+        //
+        // TXA - Transfer X to Accumulator
+        (0x8A, "TXA", 1, 2, AddressingMode::Implied),
+        //
+        // TXS - Transfer X to Stack Pointer
+        (0x9A, "TXS", 1, 2, AddressingMode::Implied),
+        //
+        // TYA - Transfer Y to Accumulator
+        (0x98, "TYA", 1, 2, AddressingMode::Implied),
     ]
     .into_iter()
     .map(OpCode::from)
